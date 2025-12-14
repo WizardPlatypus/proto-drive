20d6173c15f3b335109125d33eabd9574ed56592
WizardPlatypus <f.volik.m@gmail.com>
2025-12-14 10:52:12 +0200

    Integration testing for auth functions
    Signed-off-by: WizardPlatypus <f.volik.m@gmail.com>

---
+++ storage/tests/auth.rs
@@ -0,0 +1,34 @@
+use sqlx::{PgPool, postgres::PgPoolOptions};
+
+#[sqlx::test]
+async fn register_success(pool: PgPool) {
+    let user_id = storage::auth::register_user(&pool, "algernon", "flowers").await.unwrap();
+    assert!(!user_id.is_nil())
+}
+
+#[sqlx::test]
+async fn login_success(pool: PgPool) {
+    let login = "algernon";
+    let password = "flowers";
+    let user_id = storage::auth::register_user(&pool, login, password).await.unwrap();
+    let logged_in = storage::auth::login_user(&pool, login, password).await.unwrap();
+    assert_eq!(user_id, logged_in);
+}
+
+#[sqlx::test]
+async fn login_fail(pool: PgPool) {
+    let login = "algernon";
+    let password = "flowers";
+    let _user_id = storage::auth::register_user(&pool, login, password).await.unwrap();
+    let logged_in = storage::auth::login_user(&pool, login, "other").await;
+    assert!(logged_in.is_err());
+}
+
+#[sqlx::test]
+async fn verify_test_db(pool: PgPool) {
+    let row = sqlx::query!("SELECT current_database()")
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+    println!("connected to {}", row.current_database.unwrap());
+}
